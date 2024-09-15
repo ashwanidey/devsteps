71b45fef224b0ecb1f151b8ee3df3fb31817be54
Ashwani <ashwanidey2904@gmail.com>
2024-09-16 03:21:38 +0530

    Fix: SVG Hydration Error

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -19,11 +19,13 @@ export default function DarkModeButton() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          {theme == 'light' && <Sun className="h-[1.2rem] w-[1.2rem] " />}
+        <button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        {/* {theme == 'light' && <Sun className="h-[1.2rem] w-[1.2rem] " />}
           {theme == 'dark' &&  <Moon className="absolute h-[1.2rem] w-[1.2rem] " />}
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+         
+          <span className="sr-only">Toggle theme</span> */}
+          Theme
+        </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { initFlowbite } from "flowbite"
+
 import { useEffect } from "react"
 import DarkModeButton from "./DarkModeButton"
 
@@ -15,9 +15,7 @@ import {
 
 const NavBar = () => {
 
-  useEffect(() => {
-    initFlowbite()
-  }, [])
+  
   return (
     <>
   
@@ -64,9 +62,12 @@ const NavBar = () => {
       <DropdownMenuTrigger asChild>
       <button  type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
         <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+        <div>
+        {/* <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
+        </svg> */}
+        Menu
+        </div>
     </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
